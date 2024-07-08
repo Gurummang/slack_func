1b3f8aa2cd0e53378b2277acc74fd7d1848a790b
2W <hsp003636@gmail.com>
2024-07-08 18:09:17 +0900

    <build: init>

---
+++ src/main/java/com/GASB/slack_func/SlackFuncApplication.java
@@ -0,0 +1,13 @@
+package com.GASB.slack_func;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackFuncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackFuncApplication.class, args);
+	}
+
+}
