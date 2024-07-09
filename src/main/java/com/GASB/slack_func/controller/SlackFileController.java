8d6e42e28ac941321aa04fa040ef1c55a88440c2
2W <hsp003636@gmail.com>
2024-07-09 16:31:19 +0900

    <Modify: return type List to Map>

--- src/main/java/com/GASB/slack_func/controller/SlackFileController.java
+++ src/main/java/com/GASB/slack_func/controller/SlackFileController.java
@@ -2,16 +2,13 @@ package com.GASB.slack_func.controller;
 
 import com.GASB.slack_func.service.SlackFileService;
 import com.slack.api.methods.SlackApiException;
-import com.slack.api.model.Channel;
-import com.slack.api.model.Conversation;
-import com.slack.api.model.File;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Map;
 
 @RestController
 public class SlackFileController {
@@ -20,13 +17,13 @@ public class SlackFileController {
     private SlackFileService slackFileService;
 
     @GetMapping("/listFiles")
-    public List<File> listFiles(@RequestParam int count, @RequestParam int page) {
+    public Map<String, Object> listFiles(@RequestParam int count, @RequestParam int page) {
         try {
             return slackFileService.listFiles(count, page);
         } catch (IOException | SlackApiException e) {
             e.printStackTrace();
             // 필요하다면 적절한 응답을 반환하도록 설정
-            return null;
+            return Map.of("ok", false, "message", "error: " + e.getMessage());
         }
     }
 }
