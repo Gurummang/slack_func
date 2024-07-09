fb84a6bc944a6a31d7172933cb81dd993ddfd4b8
2W <hsp003636@gmail.com>
2024-07-09 17:57:42 +0900

    KAN-129 <fix: rename param>

--- src/main/java/com/GASB/slack_func/repository/FileUploadRepository.java
+++ src/main/java/com/GASB/slack_func/repository/FileUploadRepository.java
@@ -1,11 +1,10 @@
 package com.GASB.slack_func.repository;
 
 import com.GASB.slack_func.entity.fileUpload;
-import com.GASB.slack_func.entity.storedFiles;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface FileUploadRepository extends JpaRepository<fileUpload,Long> {
-    Optional<fileUpload> findBySaaSFileId(String SaasFileId);
+    Optional<fileUpload> findBySaasFileId(String saasFileId);
 }
