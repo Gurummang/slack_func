01f991eecbc1d1af3335b2b5e15366c9e4ea946b
2W <hsp003636@gmail.com>
2024-07-10 17:57:40 +0900

    KAN-176 <fix : change endpoint and exception>

--- src/main/java/com/GASB/slack_func/controller/SlackInfoController.java
+++ src/main/java/com/GASB/slack_func/controller/SlackInfoController.java
@@ -1,31 +1,31 @@
 package com.GASB.slack_func.controller;
 
 import com.GASB.slack_func.service.SlackChannelService;
-import com.GASB.slack_func.service.SlackFileService;
-import com.slack.api.methods.SlackApiException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.Map;
-
-@Controller
-@RequestMapping("/slack/info")
+@RestController
+@RequestMapping("/api/v1/slack")
 public class SlackInfoController {
 
-    private SlackChannelService slackChannelService;
+    private final SlackChannelService slackChannelService;
 
     @Autowired
     public SlackInfoController(SlackChannelService slackChannelService) {
         this.slackChannelService = slackChannelService;
     }
+
     @GetMapping("/channels")
     public ResponseEntity<String> fetchConversations() {
-        slackChannelService.slackFirstChannels();
-        return ResponseEntity.ok("Conversations fetched and processed successfully");
+        try {
+            slackChannelService.slackFirstChannels();
+            return ResponseEntity.ok("Conversations fetched and processed successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching conversations");
+        }
     }
 }
