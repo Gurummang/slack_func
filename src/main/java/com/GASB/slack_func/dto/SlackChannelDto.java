b0e2e5980ee8b6c3fcc63927445808c6437ec325
2W <hsp003636@gmail.com>
2024-07-10 16:57:02 +0900

    KAN-176 <add : Channel DTO>

---
+++ src/main/java/com/GASB/slack_func/dto/SlackChannelDto.java
@@ -0,0 +1,13 @@
+package com.GASB.slack_func.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SlackChannelDto {
+    private String channelId;
+    private String channelName;
+}
