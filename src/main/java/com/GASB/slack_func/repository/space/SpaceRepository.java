0b6a8681359f33223e998b628e87b37a2db35921
2W <hsp003636@gmail.com>
2024-07-11 16:40:33 +0900

    KAN-129 <fix : add annotation>

--- src/main/java/com/GASB/slack_func/repository/space/SpaceRepository.java
+++ src/main/java/com/GASB/slack_func/repository/space/SpaceRepository.java
@@ -2,6 +2,8 @@ package com.GASB.slack_func.repository.space;
 
 import com.GASB.slack_func.entity.SpaceList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SpaceRepository extends JpaRepository<SpaceList, Long> {
 }
