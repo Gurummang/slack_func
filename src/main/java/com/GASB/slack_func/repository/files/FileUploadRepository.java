132b64dccc7a5a2656f42ca013343e4b839533be
2W <hsp003636@gmail.com>
2024-07-11 16:44:19 +0900

    KAN-129 <add : annotation>

--- src/main/java/com/GASB/slack_func/repository/files/FileUploadRepository.java
+++ src/main/java/com/GASB/slack_func/repository/files/FileUploadRepository.java
@@ -2,9 +2,11 @@ package com.GASB.slack_func.repository.files;
 
 import com.GASB.slack_func.entity.fileUpload;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface FileUploadRepository extends JpaRepository<fileUpload,Long> {
     Optional<fileUpload> findBySaasFileId(String saasFileId);
 }
