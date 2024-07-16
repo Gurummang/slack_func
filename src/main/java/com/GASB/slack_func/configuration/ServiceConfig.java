e40fac8aa8c2f03f7746e7c56f4547ce33ee324e
2W <hsp003636@gmail.com>
2024-07-16 18:32:16 +0900

    KAN-108 <add : s3client config>

--- src/main/java/com/GASB/slack_func/configuration/ServiceConfig.java
+++ src/main/java/com/GASB/slack_func/configuration/ServiceConfig.java
@@ -3,13 +3,26 @@ package com.GASB.slack_func.configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class ServiceConfig {
 
+    @Value("${aws.access.key}")
+    private String awsAccessKey;
+
+    @Value("${aws.secret.key}")
+    private String awsSecretKey;
+
+    @Value("${aws.region}")
+    private String region;
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
@@ -21,5 +34,14 @@ public class ServiceConfig {
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return mapper;
+    }
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(awsAccessKey, awsSecretKey);
+
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
     }
 }
