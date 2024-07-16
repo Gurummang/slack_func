10b8d5e1d45d2ad3f3f62c8bf74cdfd36dffa945
2W <hsp003636@gmail.com>
2024-07-16 16:27:12 +0900

    KAN-108 <add : upload count top5 user>

--- src/main/java/com/GASB/slack_func/controller/SlackFileController.java
+++ src/main/java/com/GASB/slack_func/controller/SlackFileController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/file/slack")
@@ -67,6 +69,12 @@ public class SlackFileController {
                             .build()))
                     .build());
         }
+    }
+
+    @GetMapping("/top5users")
+    public ResponseEntity<List<Map.Entry<String, Long>>> getTop5UploadUsers() {
+        List<Map.Entry<String, Long>> top5Users = slackFileService.getTop5UploadUsers();
+        return ResponseEntity.ok(top5Users);
     }
 
 }
