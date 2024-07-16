dd575f8d9dbb62a3f23aecaf0f73adbb2bf66682
2W <hsp003636@gmail.com>
2024-07-16 15:12:42 +0900

    KAN-138 <fix : remove builder annotation>

--- src/main/java/com/GASB/slack_func/model/dto/SlackFileSharedEventDto.java
+++ src/main/java/com/GASB/slack_func/model/dto/SlackFileSharedEventDto.java
@@ -1,14 +1,12 @@
 package com.GASB.slack_func.model.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class SlackFileSharedEventDto {
     private String from;
     private String event;
