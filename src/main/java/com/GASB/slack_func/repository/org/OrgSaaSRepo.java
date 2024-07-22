1bbe7012bd0fd98021380b745f2a478185692629
2W <hsp003636@gmail.com>
2024-07-22 18:42:07 +0900

    KAN-103 <add : add new crud func>

--- src/main/java/com/GASB/slack_func/repository/org/OrgSaaSRepo.java
+++ src/main/java/com/GASB/slack_func/repository/org/OrgSaaSRepo.java
@@ -4,6 +4,7 @@ import com.GASB.slack_func.model.entity.OrgSaaS;
 import com.GASB.slack_func.model.entity.Saas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrgSaaSRepo extends JpaRepository<OrgSaaS, Integer> {
@@ -11,9 +12,7 @@ public interface OrgSaaSRepo extends JpaRepository<OrgSaaS, Integer> {
 
     Optional<OrgSaaS> findById(Long id);
     Optional<OrgSaaS> findBySpaceId(String spaceId);
-//
-//    Optional<OrgSaaS> findByOrgId(int orgId);
-//    Optional<OrgSaaS> findByOrgIdAndsaaSId(int orgId, int saasId);
     Optional<OrgSaaS> findByOrgIdAndSaas(int orgId, Saas saas);
+    List<OrgSaaS> findAllByOrgIdAndSaas(int orgId, Saas saas);
     Optional<OrgSaaS> findBySpaceIdAndOrgId(String spaceId, int orgId);
 }
