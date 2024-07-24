9424d475063d3146026cd58ec5e32c9d11e21bd5
2W <hsp003636@gmail.com>
2024-07-24 14:39:41 +0900

    DEV <feat : CORS Configuration>

---
+++ src/main/java/com/GASB/slack_func/configuration/CORSconfig.java
@@ -0,0 +1,23 @@
+package com.GASB.slack_func.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSconfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://grummang.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
