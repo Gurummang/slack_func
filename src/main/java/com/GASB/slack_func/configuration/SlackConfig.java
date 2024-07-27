229e05465c93f3c053c16439e57ef41ec3d99269
2W <hsp003636@gmail.com>
2024-07-27 15:48:50 +0900

    <message : OAuth test aborted >

--- src/main/java/com/GASB/slack_func/configuration/SlackConfig.java
+++ src/main/java/com/GASB/slack_func/configuration/SlackConfig.java
@@ -1,29 +1,30 @@
-package com.GASB.slack_func.configuration;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SlackConfig {
-
-    @Value("${slack.client-id}")
-    private String clientId;
-
-    @Value("${slack.client-secret}")
-    private String clientSecret;
-
-    @Value("${slack.redirect-uri}")
-    private String redirectUri;
-
-    public String getClientId() {
-        return clientId;
-    }
-
-    public String getClientSecret() {
-        return clientSecret;
-    }
-
-    public String getRedirectUri() {
-        return redirectUri;
-    }
-}
+//package com.GASB.slack_func.configuration;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SlackConfig {
+//
+//    @Value("${slack.client-id}")
+//    private String clientId;
+//
+//    @Value("${slack.client-secret}")
+//    private String clientSecret;
+//
+//    @Value("${slack.redirect-uri}")
+//    private String redirectUri;
+//
+//
+//    public String getClientId() {
+//        return clientId;
+//    }
+//
+//    public String getClientSecret() {
+//        return clientSecret;
+//    }
+//
+//    public String getRedirectUri() {
+//        return redirectUri;
+//    }
+//}
