09bcd524bc4e98e2d22e34e799aaf5ff71034d2f
2W <hsp003636@gmail.com>
2024-08-18 21:50:07 +0900

    DEV <fix : 제대로 유저 카운트 하도록 수정>

--- src/main/java/com/GASB/slack_func/repository/users/SlackUserRepo.java
+++ src/main/java/com/GASB/slack_func/repository/users/SlackUserRepo.java
@@ -17,31 +17,40 @@ public interface SlackUserRepo extends JpaRepository<MonitoredUsers, Long>{
 
 
     //DISTINCT : 중복된 값을 제거하는 키워드이다.
-    //근데 JPQL 에서는 DISTINCT를 사용할 수 없다.
-    // 그래서 nativeQuery = true로 설정하고 사용한다.
     @Query(nativeQuery = true, value =
             "SELECT " +
-                    "    u.user_name AS userName, " +
-                    "    COUNT(DISTINCT CASE WHEN dr.dlp = TRUE THEN fu.id END) AS sensitiveFilesCount, " +
-                    "    COUNT(DISTINCT CASE WHEN vr.threat_label != 'none' THEN fu.id END) AS maliciousFilesCount, " +
-                    "    MAX(fu.upload_ts) AS lastUploadedTimestamp " +
+                    "u.user_name AS userName, " +
+                    "COUNT(DISTINCT CASE WHEN dr.dlp = TRUE THEN fu.id END) AS sensitiveFilesCount, " +
+                    "COUNT(DISTINCT CASE WHEN vr.threat_label != 'none' THEN fu.id END) AS maliciousFilesCount, " +
+                    "MAX(fu.upload_ts) AS lastUploadedTimestamp " +
                     "FROM " +
                     "    monitored_users u " +
                     "JOIN " +
                     "    org_saas os ON u.org_saas_id = os.id " +
                     "JOIN " +
                     "    file_upload fu ON u.org_saas_id = fu.org_saas_id " +
+                    "JOIN " +
+                    "    stored_file sf ON sf.salted_hash = fu.salted_hash " +
+                    "JOIN " +
+                    "    activities a ON a.user_id = u.user_id AND a.saas_file_id = fu.saas_file_id " +
                     "LEFT JOIN " +
-                    "    dlp_report dr ON fu.salted_hash = dr.file_id " +
+                    "    dlp_report dr ON sf.id = dr.file_id " +
                     "LEFT JOIN " +
-                    "    vt_report vr ON fu.salted_hash = vr.file_id " +
+                    "    vt_report vr ON sf.id = vr.file_id " +
                     "WHERE " +
                     "    os.org_id = :orgId " +
                     "    AND os.saas_id = :saasId " +
+                    "    AND (vr.threat_label != 'none' OR dr.dlp = TRUE) " +
                     "GROUP BY " +
                     "    u.user_name " +
+                    "HAVING " +
+                    "    COUNT(DISTINCT CASE WHEN vr.threat_label != 'none' THEN fu.id END) > 0 " +
                     "ORDER BY " +
-                    "    (3 * COUNT(DISTINCT CASE WHEN vr.threat_label != 'none' THEN fu.id END) + COUNT(DISTINCT CASE WHEN dr.dlp = TRUE THEN fu.id END)) DESC " +
+                    "    (5 * COUNT(DISTINCT CASE WHEN vr.threat_label != 'none' THEN fu.id END) + COUNT(DISTINCT CASE WHEN dr.dlp = TRUE THEN fu.id END)) DESC " +
                     "LIMIT 5")
     List<Object[]> findTopUsers(@Param("orgId") int orgId, @Param("saasId") int saasId);
+
+
+
+
 }
