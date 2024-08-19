3235b27bb0152961c8217d5e8c2cb4c7cc233753
2W <hsp003636@gmail.com>
2024-08-19 17:19:26 +0900

    DEV <fix : 직렬화, 역직렬화를 위한 생성자 추가>

--- src/main/java/com/GASB/slack_func/annotation/JWT/JwtValidationResponse.java
+++ src/main/java/com/GASB/slack_func/annotation/JWT/JwtValidationResponse.java
@@ -1,13 +1,21 @@
 package com.GASB.slack_func.annotation.JWT;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
-@AllArgsConstructor
 public class JwtValidationResponse implements Serializable {
     private final String email;
     private final String status;
+
+    @JsonCreator
+    public JwtValidationResponse(@JsonProperty("email") String email,
+                                 @JsonProperty("status") String status) {
+        this.email = email;
+        this.status = status;
+    }
 }
+
