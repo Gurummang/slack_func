f5a8e5eb14ec8eed8347c6ef1b4c6c4722ee72a8
2W <hsp003636@gmail.com>
2024-08-28 14:46:27 +0900

    DEV <fix : 파일 이벤트 오류 수정>

--- src/main/java/com/GASB/slack_func/repository/org/OrgSaaSRepo.java
+++ src/main/java/com/GASB/slack_func/repository/org/OrgSaaSRepo.java
@@ -4,6 +4,7 @@ import com.GASB.slack_func.model.entity.OrgSaaS;
 import com.GASB.slack_func.model.entity.Saas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,7 +15,7 @@ public interface OrgSaaSRepo extends JpaRepository<OrgSaaS, Integer> {
     Optional<OrgSaaS> findById(Long id);
 
     @Query("SELECT os FROM OrgSaaS os WHERE os.spaceId = :spaceId")
-    Optional<OrgSaaS> findBySpaceIdUsingQuery(String spaceId);
+    Optional<OrgSaaS> findBySpaceIdUsingQuery(@Param("spaceId") String spaceId);
     Optional<OrgSaaS> findBySpaceId(String spaceId);
     Optional<OrgSaaS> findByOrgIdAndSpaceId(int orgId, String spaceId);
     Optional<OrgSaaS> findByOrgIdAndSaas(int orgId, Saas saas);
