33de5c90659e1837f046639866ae5237abcd63a4
2W <hsp003636@gmail.com>
2024-09-17 14:32:21 +0900

    DEV <fix : 로깅 수정>

--- src/main/java/com/GASB/slack_func/service/MessageSender.java
+++ src/main/java/com/GASB/slack_func/service/MessageSender.java
@@ -1,11 +1,13 @@
 package com.GASB.slack_func.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class MessageSender {
 
     private final RabbitTemplate rabbitTemplate;
@@ -20,16 +22,16 @@ public class MessageSender {
 
     public void sendMessage(Long message) {
         rabbitTemplate.convertAndSend(message);
-        System.out.println("Sent message to default queue: " + message);
+        log.info("Sent message to default queue: " + message);
     }
 
     public void sendGroupingMessage(Long message) {
         groupingRabbitTemplate.convertAndSend(message);
-        System.out.println("Sent message to grouping queue: " + message);
+        log.info("Sent message to grouping queue: " + message);
     }
 
     public void sendMessageToQueue(Long message, String queueName) {
         rabbitTemplate.convertAndSend(queueName, message);
-        System.out.println("Sent message to queue " + queueName + ": " + message);
+        log.info("Sent message to queue " + queueName + ": " + message);
     }
 }
